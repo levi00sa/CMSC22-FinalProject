7cce7a6189d4b9a0d7a3d0755fd4470cad9fc176
Joshua Ticot <joshuaticot@gmail.com>
2024-11-25 06:04:08 +0900

    add GameWindow.java

--- app/src/main/java/com/finalproject/app/Main.java
+++ app/src/main/java/com/finalproject/app/Main.java
@@ -1,5 +1,9 @@
 package com.finalproject.app;
 
+import com.finalproject.core.GameWindow;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        GameWindow game = new GameWindow();
+    }
 }
---
+++ app/src/main/java/com/finalproject/core/GameWindow.java
@@ -0,0 +1,24 @@
+package com.finalproject.core;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+    public GameWindow() {
+
+
+        
+        this.setTitle("FNAF GAME");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(true);
+        // this.setSize(screenSize.width, screenSize.height);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setVisible(true);
+
+        ImageIcon image = new ImageIcon("logo.png");
+
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(0,0,0));
+    }
+}
